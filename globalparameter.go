beb53af57ebb9f826a03ee87166b89c86f6551bc
conc <yueguangsenvzixiang@foxmail.com>
2015-04-29 10:57:41 +0800

    Update globalparameter.go
    change helpstr

--- globalparameter.go
+++ globalparameter.go
@@ -1,10 +1,12 @@
 package main
 
 var dataPath string = `/var/lib/noteddata/`
-var helpStr string = `notice:
+var helpStr string = `
+notice:
 	noted add "exampleKey" "exampleContent" : add a note;
 	noted del "exampleKey" :del a note;
 	noted get "exampleKey" :get a note;
 	noted append "exampleKey" :append;
 	
-	Warning : when first using this cmd, u should mkdir "/var/lib/noteddata/" and change permission.`
+Warning : when first using this cmd, u should mkdir "/var/lib/noteddata/" and change permission.
+`
